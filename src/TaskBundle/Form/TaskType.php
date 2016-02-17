c2b05411b2efecebf835e951123dcb3c3b88328b
petrero <petrero@gmail.com>
2016-02-17 21:11:26 +0200

    Run in console 'php app/console generate:doctrine:crud' produce this console dialog:
    Welcome to the Doctrine2 CRUD generator
    
    This command helps you generate CRUD controllers and templates.
    
    First, give the name of the existing entity for which you want to generate a CRUD
    (use the shortcut notation like AcmeBlogBundle:Post)
    
    The Entity shortcut name: TaskBundle:Task
    
    By default, the generator creates two actions: list and show.
    You can also ask it to generate write actions: new, update, and delete.
    
    Do you want to generate the write actions [no]? yes
    
    Determine the format to use for the generated CRUD.
    
    Configuration format (yml, xml, php, or annotation) [annotation]:
    
    Determine the routes prefix (all the routes will be mounted under this
    prefix: /prefix/, /prefix/new, ...).
    
    Routes prefix [/task]:
    
      Summary before generation
    
    You are going to generate a CRUD controller for TaskBundle:Task
    using the annotation format.
    
    Do you confirm generation [yes]?
    
      CRUD generation
    
    Generating the CRUD code: OK
    Generating the Form code: OK
    Updating the routing: OK
    
      Everything is OK! Now get to work :).

---
+++ src/TaskBundle/Form/TaskType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TaskBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('task')
+            ->add('complete')
+            ->add('created', 'datetime')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TaskBundle\Entity\Task'
+        ));
+    }
+}
