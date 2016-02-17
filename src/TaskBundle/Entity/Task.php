7e3a501c651fa9bc8c4358faf670b2712f746629
petrero <petrero@gmail.com>
2016-02-17 21:08:16 +0200

    Run in console 'php app/console generate:doctrine:entity' results in this console dialog:
     Welcome to the Doctrine2 entity generator
    
    This command helps you generate Doctrine2 entities.
    
    First, you need to give the entity name you want to generate.
    You must use the shortcut notation like AcmeBlogBundle:Post.
    
    The Entity shortcut name: TaskBundle:Task
    
    Determine the format to use for the mapping information.
    
    Configuration format (yml, xml, php, or annotation) [annotation]:
    
    Instead of starting with a blank entity, you can add some fields now.
    Note that the primary key will be added automatically (named id).
    
    Available types: array, simple_array, json_array, object,
    boolean, integer, smallint, bigint, string, text, datetime, datetimetz,
    date, time, decimal, float, binary, blob, guid.
    
    New field name (press <return> to stop adding fields): task
    Field type [string]:
    Field length [255]:
    Is nullable [false]:
    Unique [false]:
    
    New field name (press <return> to stop adding fields): complete
    Field type [string]: boolean
    Is nullable [false]:
    Unique [false]:
    
    New field name (press <return> to stop adding fields): created
    Field type [string]: datetime
    Is nullable [false]:
    Unique [false]:
    
    New field name (press <return> to stop adding fields):
    
      Entity generation
    
    > Generating entity class src/TaskBundle/Entity/Task.php: OK!
    > Generating repository class src/TaskBundle/Repository/TaskRepository.php: OK!
    
      Everything is OK! Now get to work :).

---
+++ src/TaskBundle/Entity/Task.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace TaskBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="TaskBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="task", type="string", length=255)
+     */
+    private $task;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="complete", type="boolean")
+     */
+    private $complete;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set task
+     *
+     * @param string $task
+     * @return Task
+     */
+    public function setTask($task)
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+
+    /**
+     * Get task
+     *
+     * @return string 
+     */
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    /**
+     * Set complete
+     *
+     * @param boolean $complete
+     * @return Task
+     */
+    public function setComplete($complete)
+    {
+        $this->complete = $complete;
+
+        return $this;
+    }
+
+    /**
+     * Get complete
+     *
+     * @return boolean 
+     */
+    public function getComplete()
+    {
+        return $this->complete;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Task
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
